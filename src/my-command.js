9a01ec35d68c47a785b5c524c58add43421f4337
Joey <Joey.don0905@gmail.com>
2020-08-14 21:56:16 +1000

    Update comments

--- src/my-command.js
+++ src/my-command.js
@@ -12,26 +12,32 @@ const selectedCount = selectedLayers.length;
 
 export default async function () {
   if (selectedCount === 0) {
-    // sketch.UI.alert("Failed", "No layers are selected.");
+    sketch.UI.alert("Failed", "No layers are selected.");
   } else {
     launchExport(selectedLayers);
-    // sketch.UI.alert(`Success`, `${selectedCount} layers are launched.`);
+    sketch.UI.alert(`Success`, `${selectedCount} layers are launched.`);
   }
 }
 
 async function launchExport(selectedLayers) {
+  // Loop through each selected Artboard
   for (let i = 0; i < selectedLayers.layers.length; i++) {
     let artboardName = selectedLayers.layers[i].name;
 
+    // Loop through each element in Artboards[i]
     for (let j = 0; j < selectedLayers.layers[i].layers.length; j++) {
       selectedLayers.layers[i].name = `${j.toString()}`;
+
+      // Hide all elements
       selectedLayers.layers[i].layers.map((x) => {
         x.hidden = true;
         return x;
       });
 
+      // Only un-hide the j-th element
       selectedLayers.layers[i].layers[j].hidden = false;
 
+      // Screenshot & output
       const options = {
         scales: 2,
         formats: "png",
@@ -39,6 +45,8 @@ async function launchExport(selectedLayers) {
       };
       sketch.export(selectedLayers.layers, options);
     }
+
+    // Changeback the Artboard name
     selectedLayers.layers[i].name = artboardName;
   }
 }
